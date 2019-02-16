0ba803d1085dd337cac30ff731e937efb4266b06
lonely4flow <lonelyflow@outlook.com>
2019-02-16 21:31:25 +0800

    子类进行数据配置

--- TrendRoadCharts/TrendRoadChartsSample/TrendRoadChartsSample/AskRoad/LFAskRoadViewController.swift
+++ TrendRoadCharts/TrendRoadChartsSample/TrendRoadChartsSample/AskRoad/LFAskRoadViewController.swift
@@ -15,7 +15,150 @@ class LFAskRoadViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+//    - (void)prepareCategoryList
+//    {
+//    NSMutableArray *arrayM = [NSMutableArray array];
+//    
+//    AskCategoryModel *sumCategoryModel = [AskCategoryModel new];
+//    sumCategoryModel.title = @"总和";
+//    // 大小
+//    [sumCategoryModel.itemList addObject:[AskItemModel modelWithTitle:@"大小" askTitles:@[@"大",@"小"] isShowBigRoadTxt:NO transformBlock:^(AskRoadInputModel *model, id obj) {
+//    NSString *openNums = obj[@"num"];
+//    if([NSString isEmpty:openNums]){
+//    model.isWaitOpen = YES;
+//    }else{
+//    NSArray *nums = [openNums componentsSeparatedByString:@","];
+//    NSInteger sum = [[nums valueForKeyPath:@"@sum.integerValue"] integerValue];;
+//    model.beforeTransformTxt = @(sum).stringValue;
+//    model.afterTransformTxt = sum>=23?@"大":@"小";
+//    }
+//    } fillCellBlock:^(AskRoadOutputModel *outModel,AskRoadChartCollectionViewCell *cell) {
+//    if([@"大" isEqualToString:outModel.showTxt]){
+//    // 大是红色
+//    cell.bgColor = kAskRoadRed;
+//    }else{
+//    cell.bgColor = kAskRoadBlue;
+//    }
+//    }]];
+//    // 单双
+//    [sumCategoryModel.itemList addObject:[AskItemModel modelWithTitle:@"单双" askTitles:@[@"单",@"双"] isShowBigRoadTxt:NO transformBlock:^(AskRoadInputModel *model, id obj) {
+//    NSString *openNums = obj[@"num"];
+//    if([NSString isEmpty:openNums]){
+//    model.isWaitOpen = YES;
+//    }else{
+//    NSArray *nums = [openNums componentsSeparatedByString:@","];
+//    NSInteger sum = [[nums valueForKeyPath:@"@sum.integerValue"] integerValue];;
+//    model.beforeTransformTxt = @(sum).stringValue;
+//    model.afterTransformTxt = sum%2==0?@"双":@"单";
+//    }
+//    } fillCellBlock:^(AskRoadOutputModel *outModel,AskRoadChartCollectionViewCell *cell) {
+//    if([@"双" isEqualToString:outModel.showTxt]){
+//    // 小是蓝色
+//    cell.bgColor = kAskRoadBlue;
+//    }else{
+//    cell.bgColor = kAskRoadRed;
+//    }
+//    }]];
+//    // 龙虎
+//    [sumCategoryModel.itemList addObject:[AskItemModel modelWithTitle:@"龙虎" askTitles:@[@"龙",@"虎"] isShowBigRoadTxt:NO transformBlock:^(AskRoadInputModel *model, id obj) {
+//    NSString *openNums = obj[@"num"];
+//    if([NSString isEmpty:openNums]){
+//    model.isWaitOpen = YES;
+//    }else{
+//    NSArray *nums = [openNums componentsSeparatedByString:@","];
+//    NSInteger sum = [[nums valueForKeyPath:@"@sum.integerValue"] integerValue];
+//    NSInteger firstNum = [nums.firstObject integerValue];
+//    NSInteger lastNum = [nums.lastObject integerValue];
+//    model.beforeTransformTxt = @(sum).stringValue;
+//    if(firstNum>lastNum){
+//    model.afterTransformTxt = @"龙";
+//    }else if (firstNum == lastNum){
+//    model.afterTransformTxt = @"和";
+//    }else{
+//    model.afterTransformTxt = @"虎";
+//    }
+//    
+//    }
+//    } fillCellBlock:^(AskRoadOutputModel *outModel,AskRoadChartCollectionViewCell *cell) {
+//    if([@"龙" isEqualToString:outModel.showTxt]){
+//    // 龙是红色
+//    cell.bgColor = kAskRoadRed;
+//    }else if([@"和" isEqualToString:outModel.showTxt]){
+//    // 和是绿色
+//    cell.bgColor = kAskRoadGreen;
+//    }else{
+//    cell.bgColor = kAskRoadBlue;
+//    }
+//    }]];
+//    [arrayM addObject:sumCategoryModel];
+//    
+//    NSArray *colTitles = @[@"第一球",@"第二球",@"第三球",@"第四球",@"第五球"];
+//    for(NSInteger i=0;i<colTitles.count;i++){
+//    AskCategoryModel *categoryModel = [AskCategoryModel new];
+//    categoryModel.title = colTitles[i];
+//    // 号码
+//    [categoryModel.itemList addObject:[AskItemModel modelWithTitle:@"球号" askTitles:nil isShowBigRoadTxt:YES transformBlock:^(AskRoadInputModel *model, id obj) {
+//    NSString *openNums = obj[@"num"];
+//    if([NSString isEmpty:openNums]){
+//    model.isWaitOpen = YES;
+//    }else{
+//    NSArray *nums = [openNums componentsSeparatedByString:@","];
+//    NSString *numTxt = nums[i];
+//    model.beforeTransformTxt = numTxt;
+//    model.afterTransformTxt = numTxt;
+//    }
+//    } fillCellBlock:^(AskRoadOutputModel *outModel,AskRoadChartCollectionViewCell *cell) {
+//    if([outModel.showTxt integerValue]%2==0){
+//    // 双是蓝色
+//    cell.bgColor = kAskRoadBlue;
+//    }else{
+//    cell.bgColor = kAskRoadRed;
+//    }
+//    }]];
+//    // 大小
+//    [categoryModel.itemList addObject:[AskItemModel modelWithTitle:@"大小" askTitles:@[@"大",@"小"] isShowBigRoadTxt:NO transformBlock:^(AskRoadInputModel *model, id obj) {
+//    NSString *openNums = obj[@"num"];
+//    if([NSString isEmpty:openNums]){
+//    model.isWaitOpen = YES;
+//    }else{
+//    NSArray *nums = [openNums componentsSeparatedByString:@","];
+//    NSString *numTxt = nums[i];
+//    NSInteger num = [numTxt integerValue];
+//    model.beforeTransformTxt = numTxt;
+//    model.afterTransformTxt = num>=5?@"大":@"小";
+//    }
+//    } fillCellBlock:^(AskRoadOutputModel *outModel,AskRoadChartCollectionViewCell *cell) {
+//    if([@"小" isEqualToString:outModel.showTxt]){
+//    // 小是蓝色
+//    cell.bgColor = kAskRoadBlue;
+//    }else{
+//    cell.bgColor = kAskRoadRed;
+//    }
+//    }]];
+//    // 单双
+//    [categoryModel.itemList addObject:[AskItemModel modelWithTitle:@"单双" askTitles:@[@"单",@"双"] isShowBigRoadTxt:NO transformBlock:^(AskRoadInputModel *model, id obj) {
+//    NSString *openNums = obj[@"num"];
+//    if([NSString isEmpty:openNums]){
+//    model.isWaitOpen = YES;
+//    }else{
+//    NSArray *nums = [openNums componentsSeparatedByString:@","];
+//    NSString *numTxt = nums[i];
+//    NSInteger num = [numTxt integerValue];
+//    model.beforeTransformTxt = numTxt;
+//    model.afterTransformTxt = num%2==0?@"双":@"单";
+//    }
+//    } fillCellBlock:^(AskRoadOutputModel *outModel,AskRoadChartCollectionViewCell *cell) {
+//    if([@"双" isEqualToString:outModel.showTxt]){
+//    // 双是蓝色
+//    cell.bgColor = kAskRoadBlue;
+//    }else{
+//    cell.bgColor = kAskRoadRed;
+//    }
+//    }]];
+//    [arrayM addObject:categoryModel];
+//    }
+//    self.categoryList = arrayM;
+//    }
 
     /*
     // MARK: - Navigation
